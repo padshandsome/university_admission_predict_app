4646e4005c2131cf9d15cea7075568dd5ad7e394
Mengmeng Liu <723291747@qq.com>
2021-11-12 10:17:35 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+@app.route('/index', methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        return redirect(url_for('recommendation'))
+    return render_template('index.html')
+
+@app.route('/recommendation',methods = ['GET','POST'])
+def recommendation():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('recommendation.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
