6a3d0b5af347ca0b57670a9d2a17a24b86a96bcf
Mengmeng Liu <723291747@qq.com>
2021-12-19 15:50:29 -0500

    new release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import pickle
 from model import model
+import numpy as np
 
 app = Flask(__name__)
 # We need to save the result elsewhere, cannot send the result directly to the route.
@@ -41,19 +42,47 @@ def result():
         else:
             research=0
         
-        chance_of_admit = ChoosingProcess(GPA,TOEFL,GRE,research,university_rank,personal_statement_strength,recommendation_strength)
-        return render_template('result.html', data = chance_of_admit)
+        chances = ""
+        predict_features = np.array([GRE,TOEFL,university_rank,personal_statement_strength,recommendation_strength,GPA,research]).reshape(1,-1)
+        chance_of_admit = ChoosingProcess(predict_features)
+        print(chance_of_admit==3)
+        if chance_of_admit == 3:
+            chances += "Highly"
+        elif chance_of_admit == 2:
+            chances += "Medium"
+        elif chance_of_admit == 1:
+            chances += "Low"
+        print(chances)
+        TOEFLAdvice,SOPAdvice,ResearchAdvice,RecommendationAdvice = {},{},{},{}
+        if TOEFL<100:
+            TOEFLAdvice['head'] = "TOELF"
+            TOEFLAdvice['main'] = "Your TOEFL score is lower than the average in recent years. Please note that although this score does not represent your academic level, it will affect your level of adaptation to the English academic environment in the future. If you still have time, please register for a TOEFL test and prepare carefully. Following are some websites that may help you:"
+            TOEFLAdvice['link1'] = "To register: https://www.ets.org/toefl"
+            TOEFLAdvice['link2'] = "To tips and mock exam: https://www.ets.org/s/toefl/free-practice/start.html"
+        if personal_statement_strength < 4:
+            SOPAdvice['head'] = "The Strength of SOP"
+            SOPAdvice['main'] = " Your SOP is not strong enough to persuade the school to admit you. Also, you may exaggerate the strengthyour SOP, hence we strongly recommend you have a consultation in the school’s guidance center or online. Here are some websites of tips and online consultants that may help you."
+            SOPAdvice['link1'] = "To improve: https://www.harvardsqessays.com/essay-editing/college/"
+            SOPAdvice['link2'] = "To check grammar: https://www.grammarly.com/ "  
+        if research == 0:
+            ResearchAdvice['head'] = "Research Opportunity"
+            ResearchAdvice['main'] = "If you have ever had a research opportunity, it will greatly increase your competitiveness among other students. Research opportunity includes individual academic working, working with any professor or even final project of classes. If you still cannot find any project, we have some useful websites for you:  "
+            ResearchAdvice['link1'] = "For data analysis project:https://www.kaggle.com/"
+            ResearchAdvice['link2'] = "For where to find project: https://www.quora.com/"
+        if recommendation_strength <4:
+            RecommendationAdvice['head'] = "The Strength of Letter of Recommendation:"
+            RecommendationAdvice['main'] = " Your Recommendation letter is not strong enough to persuade the school to admit you. You may persuade your professor to rewrite the letter with more words of praises, or try to find another professor. "
+            
+        return render_template('result.html', chances = chances, GPA = GPA, TOEFL = TOEFL, GRE = GRE, TOEFLAdvice = TOEFLAdvice, SOPAdvice = SOPAdvice, ResearchAdvice= ResearchAdvice,RecommendationAdvice=RecommendationAdvice)
 
 
-def ChoosingProcess(GPA,TOEFL,GRE,research,university_rank,personal_statement_strength,recommendation_strength):
+def ChoosingProcess(predict_features):
         #Predict the probability of admit
         model1 = model()
-        model1.load_model()
-        # no idea why here has eight features
-        bias = 1
-        chance_of_admit = model1.predict([[GPA,TOEFL,GRE,research,university_rank,personal_statement_strength,recommendation_strength,bias]])
-        # chance_of_admit=GPA*10+TOEFL*5+research+university_rank+recommendation_strength
-        return format(chance_of_admit[0]*100,'.2f')
+        model1.load()
+        chance_of_admit = model1.predict(predict_features.reshape(1,-1))
+        
+        return int(chance_of_admit[0])
                  
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
--- model.py
+++ model.py
@@ -1,21 +1,38 @@
 import pickle
+import numpy as np
 
 class model():
     def __init__(self) -> None:
         self.model_ = None
+        self.preprocess_ = None
 
-    def load_model(self):
-        self.model_ = pickle.load(open( 'lin_regression', 'rb'))
-    
+    def load(self):
+        self.model_ = pickle.load(open( 'model', 'rb'))
+        self.preprocess_ = pickle.load(open('scale','rb'))
+
     def predict(self,X):
         if self.model_:
+            X = self.preprocess_.transform(X)
             return self.model_.predict(X)
         else:
-            raise Exception('Model is not proper loaded.')
+            raise Exception('Model is not properly loaded.')
+
 
 if __name__ == '__main__':
-    test = model()
-    test.load_model()
+    # test = model()
+    # test.load()
 
-    X = [[0.2,0.3,0.4,1,1,9,0,1]]
-    print(test.model_.predict(X))
+    # Test_case_1 = np.array([337,118,4,4.5,4.5,9.65,1])
+    # Test_case_2 = np.array([290,100,1,1.5,2,7.56,0])
+    
+    test = model()
+    test.load()
+
+    Test_case_1 = np.array([337,118,4,4.5,4.5,9.65,1])
+
+    test.predict(Test_case_1.reshape(1,-1))
+    # Result will be 3
+    Test_case_2 = np.array([290,100,1,1.5,2,7.56,0])
+    print(test.predict(Test_case_2.reshape(1,-1)))
+    # Result will be 2
+    
